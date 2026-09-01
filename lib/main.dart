9caa32bd071cd346acd36f7230416fddfe0f75cc
Yasmeen Alshehri <s202271660@kfupm.edu.sa>
2026-09-01 18:37:18 +0300

    Task 4.2

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,14 @@
 void main() {
-  var fruitsList = ['apples', 'oranges', 'bananas'];
-  var grades = {'quiz1': 85, 'quiz2': 92};
-  var tags = {'dart', 'flutter', 'mobile', 'flutter'};
+  List<String> fruitsList = [];
+  List<String> moreFruits = ['grape', 'kiwi','mango'];
 
-  fruitsList.add('mango');
-  grades['quiz3'] = 95;
-  tags.add('dart');
+  List<String> allFruits = [
+    'watermelon',
+    if (fruitsList.isNotEmpty) ...fruitsList,
+    for (var fruit in moreFruits) fruit,
+  ];
 
-  print(fruitsList[1]);
-  print(grades['quiz2']);
-  print(tags);
-
-  // The duplicate set item is not stored twice because a Set only stores unique values.
+  print(allFruits);
 }
+
+// The spread operator (...) adds all elements of a list into another collection.
