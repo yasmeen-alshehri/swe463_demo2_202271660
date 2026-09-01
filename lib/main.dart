67183721a214e1f5efde75db043a21fb3077fcf2
Yasmeen Alshehri <s202271660@kfupm.edu.sa>
2026-09-01 19:04:28 +0300

    Task 5.3

--- lib/main.dart
+++ lib/main.dart
@@ -1,53 +1,25 @@
-abstract class Post {
-  final String author;
-
-  Post(this.author);
-
-  void render();
-}
-
-class TextPost extends Post {
-  final String text;
-
-  TextPost(String author, this.text) : super(author);
-
-  @override
-  void render() {
-    print('Text post by $author: "$text"');
+class Animal {
+  void makeSound() {
+    print('Some generic sound');
   }
 }
 
-class ImagePost extends Post {
-  final String imageUrl;
-
-  ImagePost(String author, this.imageUrl) : super(author);
-
-  @override
-  void render() {
-    print('Image post by $author at $imageUrl');
+mixin Swimmer {
+  void swim() {
+    print('I can swim!');
   }
 }
 
-// Third class: VideoPost
-class VideoPost extends Post {
-  final String videoUrl;
+class Dolphin extends Animal with Swimmer {}
 
-  VideoPost(String author, this.videoUrl) : super(author);
-
-  @override
-  void render() {
-    print('Video post by $author at $videoUrl');
-  }
-}
+class Duck extends Animal with Swimmer {}
 
 void main() {
-  List<Post> feed = [
-    TextPost('khalid', 'Flutter is amazing!'),
-    ImagePost('omar', 'https://example.com/sunset.png'),
-    VideoPost('Yasmeen', 'https://example.com/video.mp4'),
-  ];
+  final dolphin = Dolphin();
+  dolphin.swim();
+  dolphin.makeSound();
 
-  for (final post in feed) {
-    post.render();
-  }
+  final duck = Duck();
+  duck.swim();
+  duck.makeSound();
 }
