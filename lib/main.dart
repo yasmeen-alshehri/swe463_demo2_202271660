eedf52e46652afe61afeee1d4363d0bee0030794
Yasmeen Alshehri <s202271660@kfupm.edu.sa>
2026-09-01 19:01:11 +0300

    Task 5.2

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,53 @@
-class User {
-  final String username;
-  final String email;
+abstract class Post {
+  final String author;
 
-  User(this.username, this.email);
+  Post(this.author);
+
+  void render();
+}
+
+class TextPost extends Post {
+  final String text;
+
+  TextPost(String author, this.text) : super(author);
+
+  @override
+  void render() {
+    print('Text post by $author: "$text"');
+  }
+}
+
+class ImagePost extends Post {
+  final String imageUrl;
+
+  ImagePost(String author, this.imageUrl) : super(author);
+
+  @override
+  void render() {
+    print('Image post by $author at $imageUrl');
+  }
+}
+
+// Third class: VideoPost
+class VideoPost extends Post {
+  final String videoUrl;
+
+  VideoPost(String author, this.videoUrl) : super(author);
+
+  @override
+  void render() {
+    print('Video post by $author at $videoUrl');
+  }
 }
 
 void main() {
-  var user = User('ali_ux', 'ali@email.com');
-  var user2 = User('yasmeen_ux', 'yasmeen@gmail.com');
+  List<Post> feed = [
+    TextPost('khalid', 'Flutter is amazing!'),
+    ImagePost('omar', 'https://example.com/sunset.png'),
+    VideoPost('Yasmeen', 'https://example.com/video.mp4'),
+  ];
 
-  print(user.username);
-  print(user.email);
-
-  print(user2.username);
-  print(user2.email);
+  for (final post in feed) {
+    post.render();
+  }
 }
