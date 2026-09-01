da9bfd071fb4b66ef8950c2a52c46ec3b0fc0751
Yasmeen Alshehri <s202271660@kfupm.edu.sa>
2026-09-01 19:48:07 +0300

    Task 8.2

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,16 @@
-Future<String> fetchData() async {
-  await Future.delayed(const Duration(seconds: 2));
-  return 'Fetched Data';
+Stream<int> countStream(int to) async* {
+  for (int i = 1; i <= to; i++) {
+    await Future.delayed(const Duration(seconds: 1));
+    yield i;
+  }
 }
 
 Future<void> main() async {
-  print('[1] Using async/await');
-  print('[2] Fetching data...');
+  print('Start counting...');
 
-  final data = await fetchData();
-  print('[3] Result: $data');
+  await for (final count in countStream(3)) {
+    print(count);
+  }
 
-  print('[4] Using .then()');
-
-  fetchData().then((value) {
-    print('[6] .then() result: $value');
-  }).catchError((error) {
-    print('[7] Error: $error');
-  });
-
-  print('[5] Program continues after .then() call');
+  print('Done');
 }
-
-// Order: [1] → [2] → [3] → [4] → [5] → [6]
