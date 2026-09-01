7bd919edf17e9e8f53127ea5b381f6791386fc79
Yasmeen Alshehri <s202271660@kfupm.edu.sa>
2026-09-01 19:44:41 +0300

    Task 7.2

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,67 @@
-class UserProfile {
-  final String name;
-  String? bio;
+class User {
+  final String username;
+  final String email;
 
-  UserProfile(this.name, {this.bio});
+  User(this.username, this.email);
+}
 
-  String getDisplayBio() {
-    return bio ?? 'No bio provided.';
+abstract class Post {
+  final String author;
+
+  Post(this.author);
+
+  void render();
+}
+
+class TextPost extends Post {
+  final String text;
+
+  TextPost(String author, this.text) : super(author);
+
+  @override
+  void render() {
+    print('Text post by $author: "$text"');
   }
 }
 
-void main() {
-  final user1 = UserProfile('Ahmed');
-  final user2 = UserProfile('Layla', bio: 'Flutter Developer');
-  final user3 = UserProfile('Yasmeen');
+class ApiResponse<T> {
+  final bool success;
+  final T? data;
+  final String? errorMessage;
 
-  print(user1.getDisplayBio());
-  print(user2.getDisplayBio());
-  print(user3.getDisplayBio());
+  ApiResponse(this.success, {this.data, this.errorMessage});
+}
+
+void main() {
+  final userResponse = ApiResponse<User>(
+    true,
+    data: User('khalid', 'khalid@example.com'),
+  );
+
+  print(userResponse.data?.username);
+
+  final postResponse = ApiResponse<Post>(
+    true,
+    data: TextPost('Turki', 'Flutter is awesome'),
+  );
+
+  if (postResponse.success) {
+    postResponse.data?.render();
+  } else {
+    print('Response failed: ${postResponse.errorMessage}');
+  }
+
+  final errorResponse = ApiResponse<User>(
+    false,
+    errorMessage: 'Unable to load user',
+  );
+
+  print(errorResponse.errorMessage);
+
+  final intResponse = ApiResponse<int>(
+    true,
+    data: 200,
+  );
+
+  print(intResponse.data);
 }
